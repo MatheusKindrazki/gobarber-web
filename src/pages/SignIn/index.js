6d4d5da701ff517e835f6a0e6175e79c2ab06834
Matheus Kindrazki <matheus@mentores.com.br>
2019-09-15 10:44:46 -0300

    validação de campos do formulário

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido!')
+    .required(),
+  password: Yup.string().required(),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -15,7 +21,7 @@ export default function SignIn() {
     <>
       <img src={logo} alt="gobarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua Senha" />
 
