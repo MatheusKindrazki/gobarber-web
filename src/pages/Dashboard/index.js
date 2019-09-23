b9f2820f0c10f4de9de4a8be088644eb6011358e
Matheus Kindrazki <matheuskindrazki@gmail.com>
2019-09-23 18:14:13 -0300

    estilização dos cards

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft color="#FFF" size={36} />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight color="#FFF" size={36} />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Matheus Kindrazki</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>EM aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Matheus Kindrazki</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
